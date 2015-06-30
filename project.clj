f8efd5977b6b8b218aab1790bab37b30eb22726d
Sebastian Bensusan <sbensu@gmail.com>
2015-06-30 15:21:40 +0200

    Changed project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.omcljs/om "0.9.0-SNAPSHOT"
+(defproject bensu/om "0.8.9-SNAPSHOT"
   :description "ClojureScript interface to Facebook's React"
   :url "http://github.com/swannodette/om"
   :license {:name "Eclipse"
